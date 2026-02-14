3ff97da7eda412ce4a582fb99f50a941a9de2c99
timofijlisovij-source <timofijlisovij@gmail.com>
2026-02-14 16:25:49 +0200

    Create part_2.py

---
+++ part_2.py
@@ -0,0 +1,17 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    
+    if (
+        min < 1 or
+        max > 1000 or
+        min > max or
+        quantity < 0 or
+        quantity > (max - min + 1)
+    ):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+
+    return sorted(numbers)
+ 
