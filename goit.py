91f310ebd295ea4aabd29cd3703aa6f420c9ca72
timofijlisovij-source <timofijlisovij@gmail.com>
2026-02-14 20:03:04 +0200

    gdfs

--- goit.py
+++ goit.py
@@ -1,6 +1,10 @@
 from datetime import datetime
-date = input("enter the date:")
-date_value = datetime.strptime(date,"%Y-%m-%d")
-Now= datetime.now()
-days = (Now.toordinal())-(date_value.toordinal())
-print(days)
+def get_days_from_today(date):
+    try:
+        date_value = datetime.strptime(date,"%Y-%m-%d")
+        Now= datetime.now()
+        days = (Now.toordinal())-(date_value.toordinal())
+        return days
+    except ValueError:
+        return "invalid data format"
+print(get_days_from_today("2010-12-5"))
