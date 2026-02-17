51e93bb266a1b96262710312cbf3ed53d18d6a4d
timofijlisovij-source <timofijlisovij@gmail.com>
2026-02-17 21:40:29 +0200

    Update goit.py

--- goit.py
+++ goit.py
@@ -1,10 +1,15 @@
 from datetime import datetime
-def get_days_from_today(date):
+
+
+def get_days_from_today(date: str) -> int | str:
     try:
-        date_value = datetime.strptime(date,"%Y-%m-%d")
-        Now= datetime.now()
-        days = (Now.toordinal())-(date_value.toordinal())
+        date_value = datetime.strptime(date, "%Y-%m-%d")
+        now = datetime.now()
+        days = now.toordinal() - date_value.toordinal()
         return days
     except ValueError:
         return "invalid data format"
-print(get_days_from_today("2010-12-5"))
+
+
+if __name__ == "__main__":
+    print(get_days_from_today("2010-12-5"))
